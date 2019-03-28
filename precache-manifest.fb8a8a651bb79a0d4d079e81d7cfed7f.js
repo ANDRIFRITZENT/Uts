b962a9266b092629e5865736e534afd97d8e6cd0
ANDRIFRITZENT <andryfritzent05@gmail.com>
2019-03-28 08:33:08 +0700

    Updates

---
+++ precache-manifest.fb8a8a651bb79a0d4d079e81d7cfed7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0018df272d37f076af5d",
+    "url": "/Uts/static/js/runtime~main.ebdc4c1e.js"
+  },
+  {
+    "revision": "f1fee713915a101282ad",
+    "url": "/Uts/static/js/main.4d9e4dee.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/Uts/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "f1fee713915a101282ad",
+    "url": "/Uts/static/css/main.86185c20.chunk.css"
+  },
+  {
+    "revision": "b5d599af55de49d4be88884dcf946238",
+    "url": "/Uts/index.html"
+  }
+];
